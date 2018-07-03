783ba41cbf567d30acecd186da00f303f44c3d3c
Igor Pavlichenko <igor.pavlichenko@hplaptop.dev2grow>
2018-07-03 02:46:18 +0100

    A better implementation using rxjs

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
 /**
  * We want a footer pagination so the user can navigate through the webapp.
  *
@@ -11,23 +13,23 @@
  *
  * @param currentPage _actual_ page
  * @param totalPages _total_ available pages
- * @param boundaries how many pages we want to link in the beginning and in the end
- * @param around how many pages we want to link before and after the actual page
+ * @param inputBoundaries how many pages we want to link in the beginning and in the end
+ * @param inputAround how many pages we want to link before and after the actual page
  */
 export const pagination = (
   currentPage: number,
   totalPages: number,
-  boundaries: number,
-  around: number,
+  inputBoundaries: number,
+  inputAround: number,
 ) => {
   if (!Number.isSafeInteger(currentPage)
     || !Number.isSafeInteger(totalPages)
     || !Number.isSafeInteger(totalPages)
-    || !Number.isSafeInteger(around)) {
+    || !Number.isSafeInteger(inputAround)) {
     return 'v8 limitation with Number.MAX_SAFE_INTEGER';
   }
-  if (totalPages < 1 || boundaries < 0 || around < 0) {
-    return '"boundaries" & "around" should not be negative'
+  if (totalPages < 1 || inputBoundaries < 0 || inputAround < 0) {
+    return '"inputBoundaries" & "inputAround" should not be negative'
       + 'AND "totalPages" should be greater than 0';
   }
   if (currentPage < 1 || currentPage > totalPages) {
@@ -37,184 +39,126 @@ export const pagination = (
   const MAX_ALLOWED_AROUND = 20;
   // do not allow big intervals of pages to be displayed
   // technically it is possible tho..
-  let boundariesApplied = boundaries;
-  if (boundaries > MAX_ALLOWED_BOUNDARIES) {
-    boundariesApplied = MAX_ALLOWED_BOUNDARIES;
-    // console.log('using MAX allowed boundary: ', MAX_ALLOWED_BOUNDARIES);
+  let boundaries = inputBoundaries;
+  if (inputBoundaries > MAX_ALLOWED_BOUNDARIES) {
+    boundaries = MAX_ALLOWED_BOUNDARIES;
   }
-  let aroundApplied = around;
-  if (around > MAX_ALLOWED_AROUND) {
-    // console.log('using MAX allowed around: ', MAX_ALLOWED_AROUND);
-    aroundApplied = MAX_ALLOWED_AROUND;
+  let around = inputAround;
+  if (inputAround > MAX_ALLOWED_AROUND) {
+    around = MAX_ALLOWED_AROUND;
   }
 
-  // calculate boundaries and around intervals using simple math
-  const leftSideBoundary = {
-    start: boundariesApplied ? 1 : 0,
-    // if boundaries are bigger than current page - limit the interval to current page
-    end: boundariesApplied ? (
-      (boundariesApplied < currentPage)
-        ? boundariesApplied
-        : currentPage - 1)
-      : 0,
-  };
-  const leftSideAround = {
-    // if boundaries are bigger than current page - care to avoid starting at negative numbers
-    start: aroundApplied ? (
-      (aroundApplied < currentPage)
-        ? currentPage - aroundApplied
-        : 1)
-      : 0,
-    end: aroundApplied ? currentPage - 1 : 0,
-  };
+  // basic intervals calculations, just to avoid inefficient loops
+  let boundariesIntervals = { left: null, right: null };
+  if (boundaries) {
+    boundariesIntervals = {
+      left: {
+        start: 1,
+        end: boundaries,
+      },
+      right: {
+        // +1 because pages start at 1, not 0
+        start: (1 + totalPages) - boundaries,
+        end: totalPages,
+      },
+    };
+  }
+  let aroundIntervals = { left: null, right: null };
+  if (around) {
+    aroundIntervals = {
+      left: {
+        start: currentPage - around,
+        end: currentPage,
+      },
+      right: {
+        start: currentPage,
+        end: currentPage + around,
+      },
+    };
+  }
 
-  const rightSideBoundary = { start: 0, end: 0 };
-  if (boundariesApplied) {
-    // if current page isn't the last
-    if (currentPage !== totalPages) {
-      // then boundary starts n pages before totalPages which is the end
-      rightSideBoundary.start = totalPages - (boundariesApplied - 1);
-      // but if start is less than currentPage then it starts right after currentPage
-      if (rightSideBoundary.start < currentPage) {
-        rightSideBoundary.start = currentPage + 1;
-      }
-      // and ends at the last page which is = totalPages
-      rightSideBoundary.end = totalPages;
-      // but if after adding the amount, the END exceeds totalPages then just
-      // make it = totalPages
+  // stick all available page between
+  // 'worst-case-scenario' boundaries and around ranges
+  // in a set to discard duplicates
+  const pagesSet = new Set<number>([currentPage]);
+  [
+    boundariesIntervals.left,
+    aroundIntervals.left,
+    aroundIntervals.right,
+    boundariesIntervals.right,
+  ].map((interval) => { // loop over our 'dirty' intervals and
+    if (interval) { // if interval actually exists
+      // naughty rxjs.. almost got me here
+      // this is the part where we must enforce intervals that are
+      // enough to cover whole range AND do not exceed boundaries of 1 - totalPages
+      // rxjs's 'range(start, counter)' observable emits incremented numbers
+      // starting at 'start'. The quantity of emitted numbers equals to 'counter'
+      // therefore those values must be calculated very carefully
+      const isEndOutsideBoundaries = interval.end > totalPages;
+      const isStartOutsideBoundaries = interval.start < 1;
+      const start = isStartOutsideBoundaries ? 1 : interval.start;
+      const end = isEndOutsideBoundaries ? totalPages : interval.end;
+      const count = (end + 1) - (start - 1);
+      range(start, count)
+        .pipe(
+          // filter pages out of bounds
+          filter(x => x >= 1 && x <= totalPages),
+          // and those that are [for some reason, looking at you -> rxjs]
+          // out of their own intervals
+          filter(x => interval.start && x <= interval.end),
+      )
+        // add them to array after filters
+        .subscribe(x => pagesSet.add(x));
     }
-  }
+  });
 
-  const rightSideAround = { start: 0, end: 0 };
-  if (aroundApplied) {
-    // if current page isn't the last page
-    if (currentPage !== totalPages) {
-      // then around starts at the page after currentPage
-      rightSideAround.start = 1 + currentPage;
-      // and ends specified pages after it
-      rightSideAround.end = currentPage + aroundApplied;
-      // but if after adding the amount, the END exceeds totalPages then just
-      // make it = totalPages
-      if (rightSideAround.end > totalPages) rightSideAround.end = totalPages;
+  const uniquePagesSorted = [...pagesSet].sort((a, b) => a - b);
+  // console.log('pagesSet: ', uniquePagesSorted);
+
+  let finalResult = '';
+
+  const { length } = uniquePagesSorted;
+  uniquePagesSorted.map((page, i) => {
+    const prevPage = uniquePagesSorted[i - 1];
+    const nextPage = uniquePagesSorted[i + 1];
+
+    let pageStr = ` ${page} `;
+
+    // if it's the currentPage - wrap it in [ ]
+    if (page === currentPage) {
+      pageStr = ` [${page}] `;
     }
-  }
 
-  // get boundary pages for both sides
-  // boundaries are sliced from external limits (far from current page)
-  const boundariesLeftSide = [];
-  if (leftSideBoundary.start && leftSideBoundary.end) {
-    for (let i = leftSideBoundary.start; i <= leftSideBoundary.end; i++) {
-      boundariesLeftSide.push(i);
-    }
-  }
-
-  const boundariesRightSide = [];
-  if (rightSideBoundary.start && rightSideBoundary.end) {
-    for (let i = rightSideBoundary.start; i <= rightSideBoundary.end; i++) {
-      boundariesRightSide.push(i);
-    }
-  }
-
-  // get pages around the current page
-  const aroundLeftSide = [];
-  if (leftSideAround.start && leftSideAround.end) {
-    for (let i = leftSideAround.start; i <= leftSideAround.end; i++) {
-      aroundLeftSide.push(i);
-    }
-  }
-
-  const aroundRightSide = [];
-  if (rightSideAround.start && rightSideAround.end) {
-    for (let i = rightSideAround.start; i <= rightSideAround.end; i++) {
-      // console.log('pushing i: ', i);
-      aroundRightSide.push(i);
-    }
-  }
-
-  // filter duplicates and sort them
-  const leftSideNoDuplicates = [...new Set<number>(
-    [...boundariesLeftSide, ...aroundLeftSide].sort((a, b) => a - b),
-  )];
-  const rightSideNoDuplicates = [...new Set<number>(
-    [...boundariesRightSide, ...aroundRightSide].sort((a, b) => a - b),
-  )];
-
-  // add '...' when boundaries are hidden
-  let leftSideString = '';
-  if (leftSideNoDuplicates.length > 0) {
-    // if there are no boundaries pages to left of current page
-    if (!boundariesLeftSide.length) {
-      // then if by going back 'around' pages from currentPage we
-      // don't break the inferior limit
-      if (currentPage - aroundApplied > 1) {
-        // means there are pages to omit
-        leftSideString += '...';
+    // if it's not the page #1
+    if (page !== 1) {
+      // AND it's at index 0 - means there are pages hidden before it
+      if (i === 0) {
+        pageStr = ` ... ${pageStr} `;
+      } else if (page !== (prevPage + 1)) {
+      // if the previous page broke sequence
+        pageStr = ` ... ${pageStr} `;
       }
     }
-    // build the string that precedes the [currentPage]
-    leftSideNoDuplicates.map((page, i) => {
-      // the logic is
-      // while iterating pages to concatenate them all in a single string
-      // to compare previous page to current and
-      // if the difference between prev page and current is greater than 1
-      // means it's a sequence breaking - add the three dots
-      const prevPage = leftSideNoDuplicates[i - 1];
-      let joinToken = ' ';
-      if (prevPage && page - prevPage > 1) {
-        joinToken = ' ... ';
-      }
-      leftSideString += `${joinToken}${page}`;
-    });
-    // add '...' when around are hidden
-    // if there are no pages AROUND to left side
-    if (!aroundLeftSide.length) {
-      // check for boundaries
-      // if closest page from boundaries is not current page
-      if (((boundariesLeftSide.slice(-1)[0] + 1) !== currentPage)) {
-        leftSideString += ' ...';
-      }
 
-    }
-  }
-
-  let rightSideString = '';
-  // if there are pages to be printed AFTER currentPage
-  if (rightSideNoDuplicates.length > 0) {
-    // if there is stuff to concat after currentPage and it's not 'pages around'
-    if (!aroundRightSide.length) {
-      // then we must check whether the first page of the array comes
-      // immediately after current page OR there are pages in between that
-      // should be omitted with ...
-      if (currentPage + 1 !== rightSideNoDuplicates[0]) {
-        rightSideString += ' ...';
+    // if it's not the last page
+    if (page !== totalPages) {
+      // if it's at the last index of array - means there are hidden pages after it
+      if (i === length - 1) {
+        pageStr = ` ${pageStr} ...`;
+      } else if (i < (length - 1) && page !== (nextPage - 1)) {
+      // if the next page will break sequence
+        pageStr = ` ${pageStr} ...`;
       }
     }
-    // build the string that follows [currentPage]
-    rightSideNoDuplicates.map((page, i) => {
-      const prevPage = rightSideNoDuplicates[i - 1];
-      let joinToken = ' ';
-      if (prevPage && page - prevPage !== 1) {
-        joinToken = ' ... ';
-      }
-      rightSideString += `${joinToken}${page}`;
-    });
-    // if there are no boundaries
-    if (!boundariesRightSide.length) {
-      // then there are around
-      // only omit pages if after adding 'around' to current page number
-      // it doesn't exceeds totalPages limit
-      if (currentPage + aroundApplied < totalPages) {
-        rightSideString += ' ...';
-      }
-    }
-  }
 
+    finalResult += pageStr;
+  });
 
-  const finalResult = `${leftSideString} [${currentPage}]${rightSideString}`;
-
-
-  return finalResult.trim();
+  // replace duplicated spaces with single space and remove spaces from edges
+  finalResult = finalResult
+    .replace(/\s+/g, ' ') // replace extended white spaces with single space
+    .replace('... ...', '...') // replace duplicated dots ... ...
+    .replace('... ...', '...') // this is a bit ugly, I know :\
+    .trim();
+  return finalResult;
 };
-
-
-console.log(pagination(1, -5, -5, -5));
